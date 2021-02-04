f5bb01be964911538687a57a144d1570aa384466
Alvaro Lupa <alvaro.lupa@gmail.com>
2021-02-04 20:37:04 -0300

    Patient new data structure for post

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -32,48 +32,59 @@ class PatientController extends Controller
      */
     public function create()
     {
+        $valueSetUrl = 'http://fhir-ssiq.cens.cl/ssiq/fhir/';
+
         /* Nivel de Instrucción */
-        $url = $this->getUrlBase().'ValueSet/instruction-level';
+        // $url = $this->getUrlBase().'ValueSet/instruction-level';
+        $url = $valueSetUrl.'ValueSet/instruction-level';
         $response = Http::withToken($this->getToken())->get($url);
         $instructionLevel = $response->json()['compose']['include'][0]['concept'];
 
         /* Identidad de genero */
-        $url = $this->getUrlBase().'ValueSet/gender-identity';
+        // $url = $this->getUrlBase().'ValueSet/gender-identity';
+        $url = $valueSetUrl.'ValueSet/gender-identity';
         $response = Http::withToken($this->getToken())->get($url);
         $genderIdentities = $response->json()['compose']['include'][0]['concept'];
 
         /* Genero */
-        $url = $this->getUrlBase().'ValueSet/administrative-gender';
+        // $url = $this->getUrlBase().'ValueSet/administrative-gender';
+        $url = $valueSetUrl.'ValueSet/administrative-gender';
         $response = Http::withToken($this->getToken())->get($url);
         $administrativeGenders = $response->json()['compose']['include'][0]['concept'];
 
         /* Previsión */
-        $url = $this->getUrlBase().'ValueSet/health-insurance';
+        // $url = $this->getUrlBase().'ValueSet/health-insurance';
+        $url = $valueSetUrl.'ValueSet/health-insurance';
         $response = Http::withToken($this->getToken())->get($url);
         $previciones = $response->json()['compose']['include'][0]['concept'];
 
         /* Comunas */
-        $url = $this->getUrlBase().'ValueSet/commune';
+        // $url = $this->getUrlBase().'ValueSet/commune';
+        $url = $valueSetUrl.'ValueSet/commune';
         $response = Http::withToken($this->getToken())->get($url);
         $communes = $response->json()['compose']['include'][0]['concept'];
 
         /* Regiones */
-        $url = $this->getUrlBase().'ValueSet/region';
+        // $url = $this->getUrlBase().'ValueSet/region';
+        $url = $valueSetUrl.'ValueSet/region';
         $response = Http::withToken($this->getToken())->get($url);
         $regions = $response->json()['compose']['include'][0]['concept'];
 
         /* Pueblos originarios */
-        $url = $this->getUrlBase().'ValueSet/aboriginal-community';
+        // $url = $this->getUrlBase().'ValueSet/aboriginal-community';
+        $url = $valueSetUrl.'ValueSet/aboriginal-community';
         $response = Http::withToken($this->getToken())->get($url);
         $aboriginals = $response->json()['compose']['include'][0]['concept'];
 
         /* Vias de acceso */
-        $url = $this->getUrlBase().'ValueSet/street-type';
+        // $url = $this->getUrlBase().'ValueSet/street-type';
+        $url = $valueSetUrl.'ValueSet/street-type';
         $response = Http::withToken($this->getToken())->get($url);
         $streetTypes = $response->json()['compose']['include'][0]['concept'];
 
         /* Estado civil */
-        $url = $this->getUrlBase().'ValueSet/marital-status';
+        // $url = $this->getUrlBase().'ValueSet/marital-status';
+        $url = $valueSetUrl.'ValueSet/marital-status';
         $response = Http::withToken($this->getToken())->get($url);
         $maritalStatus = $response->json()['compose']['include'][0]['concept'];
 
@@ -255,7 +266,60 @@ class PatientController extends Controller
               ],
             ];
 
-        //dd(json_encode($data));
+        /* Nueva estructura data */
+        $data = [
+          'address' => [
+            0 => [
+              'text' => $request->input('streetName').' '.$request->input('addressNumber').($request->input('addressApartament') ? ' Depto. '.$request->input('addressApartament').', ' : ' ').$request->input('poblacion'),
+            ],
+          ],
+          'birthDate' => $request->input('birthdate'),
+          'extension' => [
+            0 => [
+              'url' => 'https://fhir-ssiq.cens.cl/ssiq/fhir/ValueSet/health-insurance',
+              'valueString' => $request->input('prevision'),
+            ],
+          ],
+          'gender' => $request->input('gender'),
+          'identifier' => [
+            0 => [
+              'use' => 'usual',
+                          'type' => [
+                                'coding' => [
+                                      0 => [
+                                            'system' => 'http://terminology.hl7.org/CodeSystem/v2-0203',
+                                            'code' => $request->input('id_type'),
+                                      ],
+                                ],
+                          ],
+              'value' => $request->input('identifier'),
+            ],
+          ],
+          'name' => [
+            0 => [
+              '_family' => [
+                'extension' => [
+                  0 => [
+                    'url' => 'http://hl7.org/fhir/StructureDefinition/humanname-fathers-family',
+                    'valueString' => $request->input('fathers_family'),
+                  ],
+                  1 => [
+                    'url' => 'http://hl7.org/fhir/StructureDefinition/humanname-mothers-family',
+                    'valueString' => $request->input('mothers_family'),
+                  ],
+                ],
+              ],
+              'given' => [
+                0 => $request->input('name'),
+              ],
+              'text' => $request->input('name').' '.$request->input('fathers_family').' '.$request->input('mothers_family'),
+              'use' => 'official',
+            ],
+          ],
+          'resourceType' => 'Patient',
+        ];
+
+        // dd(json_encode($data));
         $response = Http::withToken($this->getToken())->post($url, $data);
 
         return redirect()->route('patient.index');
