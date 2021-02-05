7001fd004932874e5fc54dc3d5763d4bc231118f
AquaroTorres <alvarotorres@gmail.com>
2021-02-04 22:24:44 -0300

    Uncomment Google Trait

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Auth::routes();
+//
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
 
