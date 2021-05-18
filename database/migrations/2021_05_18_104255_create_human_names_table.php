b117c7e3f416a392e88f0ab364a1414aed11f6d4
GermanZun <german.zuniga@gmail.com>
2021-05-17 21:19:33 -0400

    fix order migration

--- database/migrations/2021_05_18_104255_create_human_names_table.php
+++ database/migrations/2021_05_18_104255_create_human_names_table.php
@@ -23,12 +23,12 @@ class CreateHumanNamesTable extends Migration
             $table->string('mothers_family')->nullable();
             $table->string('prefix')->nullable();
             $table->string('suffix')->nullable();
-           /**$table->foreignId('users_id')->nullable(); */ 
+            $table->foreignId('user_id')->nullable();
 
             $table->timestamps();
 
             $table->foreign('human_names_id')->references('id')->on('human_names');
-           /** $table->foreign('users_id')->references('id')->on('users'); */
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
