2ef584fe84c9bdfac3afe7e85fb8dee0b55aa2f4
inguer <inguerescudero@gmail.com>
2021-05-28 13:55:22 -0400

    Table Identifier + CodConIdentifierTypes/Seeder/Model

---
+++ database/migrations/2014_05_09_100750_create_cod_con_identifiers_types_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCodConIdentifiersTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cod_con_identifiers_types', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('cod_con_identifiers_types_id')->nullable();
+            $table->foreignId('coding_id')->nullable();
+            $table->string('text')->nullable();;
+
+            $table->foreign('cod_con_identifiers_types_id')->references('id')->on('cod_con_identifiers_types');
+            $table->foreign('coding_id')->references('id')->on('coding');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cod_con_identifiers');
+    }
+}
