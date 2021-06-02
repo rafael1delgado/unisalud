530f296a90c9940b49715788e250912ba79cdd3c
AquaroTorres <alvarotorres@gmail.com>
2021-06-02 13:16:56 -0400

    FQ home & login

--- resources/views/fq/home.blade.php
+++ resources/views/fq/home.blade.php
@@ -4,9 +4,16 @@
 
 <div class="jumbotron mt-3">
     <div class="container">
-        <h class="display-4">Portal de Salud</h1>
-        <p class="lead">Bienvenido al portal de salud de la región de Tarapacá.<br>
-            Acá encontrarás la información que esté disponible para tí.</p>
+        <h1 class="display-4">Fibrosis Quística</h1>
+        <div>
+        <img src="{{ asset('images/fq.png') }}" class="img-thumbnail" 
+        alt="Imagen de Fibrosis Quísitica">
+        </div>
+        
+        <br> <br> 
+        <p class="lead">Bienvenido al portal UNISALUD de apoyo a los paciente con Fibrosis Quística.<br>
+            Compromiso adquirido con las familias por el director del Servicio de Salud de Iquique.<br></p>
+            <h5><strong>Jorge Galleguillos Möller</strong></h5>
     </div>
 </div>
 
