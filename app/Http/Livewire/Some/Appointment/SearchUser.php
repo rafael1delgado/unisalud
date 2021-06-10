f951c5c5cc5193ed58103cce6a341ba1730415dc
GermanZun <german.zuniga@gmail.com>
2021-06-10 11:43:34 -0400

    add model appointment, livewire some

---
+++ app/Http/Livewire/Some/Appointment/SearchUser.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Some\Appointment;
+
+use Livewire\Component;
+
+class SearchUser extends Component
+{
+    public function render()
+    {
+        return view('livewire.some.appointment.search-user');
+    }
+}
