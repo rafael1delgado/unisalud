b87831129b979112049607979c7e97d77fc92c81
GermanZun <german.zuniga@gmail.com>
2021-06-14 10:27:36 -0400

    some folder

---
+++ app/Http/Controllers/Some/AppointmentController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Some;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AppointmentController extends Controller
+{
+    public function openAgenda($from, $to)
+    {
+        
+    }
+}
