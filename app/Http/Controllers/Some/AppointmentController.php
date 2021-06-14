b6590b427a2fe5f1bd6ec53892cd661245ad417a
GermanZun <german.zuniga@gmail.com>
2021-06-14 11:11:54 -0400

    open agenda

--- app/Http/Controllers/Some/AppointmentController.php
+++ app/Http/Controllers/Some/AppointmentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Some;
 
 use App\Http\Controllers\Controller;
+use App\Models\MedicalProgrammer\TheoreticalProgramming;
 use Illuminate\Http\Request;
 
 class AppointmentController extends Controller
@@ -12,9 +13,15 @@ class AppointmentController extends Controller
         return view('some.appointment');
     }
 
-
     public function openAgenda($from, $to)
     {
+        $theoreticalProgramming = TheoreticalProgramming::query()
+//            ->where('start_date', $from)
+//            ->where('end_date', $to)
+
+            ->get();
+
+        dd($theoreticalProgramming);
 
     }
 }
