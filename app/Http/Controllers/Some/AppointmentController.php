36372f52c259327779ae345034754fd26c83db7e
Esteban <sick_iqq@hotmail.com>
2021-06-14 16:11:22 -0400

    some: add aperturar agenda

--- app/Http/Controllers/Some/AppointmentController.php
+++ app/Http/Controllers/Some/AppointmentController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\Some;
 
 use App\Http\Controllers\Controller;
 use App\Models\MedicalProgrammer\TheoreticalProgramming;
+use App\Models\User;
+
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class AppointmentController extends Controller
 {
@@ -23,5 +26,22 @@ class AppointmentController extends Controller
 
         dd($theoreticalProgramming);
 
+    }
+
+    public function openTProgrammerView(Request $request){
+      $theoreticalProgrammings = null;
+
+      if($request){
+        if ($request->user_id != null) {
+          // $monday = Carbon::parse($request->date)->startOfWeek();
+          // $sunday = Carbon::parse($request->date)->endOfWeek();
+          $theoreticalProgrammings = TheoreticalProgramming::where('user_id',$request->user_id)
+                                                           // ->whereBetween('start_date',[$monday,$sunday])
+                                                           ->get();
+                                                           // dd($request->user_id, $theoreticalProgrammings);
+        }
+      }
+
+      return view('some.open_tprogrammer',compact('request','theoreticalProgrammings'));
     }
 }
