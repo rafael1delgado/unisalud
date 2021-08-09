76464542d4dbcf85d105287c9bc04abbeb10ac8f
GermanZun <german.zuniga@gmail.com>
2021-08-09 18:36:10 -0400

    refactor identifiers , fix dv rut

--- resources/views/livewire/some/search-user.blade.php
+++ resources/views/livewire/some/search-user.blade.php
@@ -61,7 +61,7 @@
             </div>
 
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal"> faclose Cerrar
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar
                 </button>
             </div>
         </div>
