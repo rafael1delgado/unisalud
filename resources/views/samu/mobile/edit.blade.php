d951359da53a6d9a3ec31ff625d4556bcabee3c4
AdrianaOL <oviedolopezadriana13@gmail.com>
2021-08-17 10:58:16 -0400

    mobile-samu

---
+++ resources/views/samu/mobile/edit.blade.php
@@ -0,0 +1,82 @@
+@extends('layouts.app')
+
+@section('content')
+
+<style>
+ .button1{
+     margin-top:30px;
+     }
+</style>
+
+<div class="card mb-3">
+    <div class="card-body">
+   
+
+            <div class="col-md-6">
+                <h3 class="mb-3"><i class="fas fa-plus"></i> Editar Movil</h3>
+            </div>
+            <hr>
+            <div class="form-row">
+        
+                <fieldset class="form-group col-8 col-md-3">
+                    <label for="for_run">Número de Amb </label>
+                    <input type="hidden" class="form-control" id="for_id" name="id" value="">
+                    <input type="number" max="50000000" class="form-control" id="for_run" name="run" value="2">
+                </fieldset>
+
+                <fieldset class="form-group col-12 col-md-4">
+                    <label for="empresa">Tipo</label>
+                    <select class="form-control" name="region_id" id="regiones">
+                        <option>Movil 2</option>
+                                    <option value="1" >Movil 1</option>
+                                    <option value="2" >Movil  2</option>
+                                    <option value="3" >Movil  3</option>
+                                    <option value="4" >Movil  4</option>
+                                    <option value="5" >Movil  5</option>
+                                    <option value="3" >Movil  6</option>
+                                    <option value="4" >Movil  7</option>
+                                    <option value="5" >Movil  8</option>
+                                    <option value="1" >Movil 9</option>
+                                    <option value="2" >Movil  10</option>
+                                    <option value="3" >Movil  11</option>
+                                    <option value="4" >Movil  12</option>
+                                    <option value="5" >Movil  13</option>
+                                    <option value="3" >Movil  14</option>
+                                    <option value="4" >Movil  15</option>
+                                    <option value="5" >Movil  16</option>
+                                </select>
+                </fieldset>
+                
+                <fieldset class="form-group col-12 col-md-5">
+                    <label for="empresa">Estado</label>
+                    <select class="form-control" name="region_id" id="regiones">
+                        <option>Activo</option>
+                                    <option value="1" >Activo</option>
+                                    <option value="2" >Inactivo</option>
+                                </select>
+                </fieldset>
+
+            </div>
+
+
+            <hr>
+
+            <div class="form-row">
+
+                <fieldset class="form-group col-12 col-md-2 ">
+                
+                <button type="submit" class="btn btn-primary button" >Guardar</button>
+     
+                </fieldset>
+            </div>
+
+            
+        </div>
+        </div>
+    
+
+
+
+
+
+@endsection
---
+++ resources/views/samu/mobile/index.blade.php
@@ -0,0 +1,81 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+
+<h3 class="mb-3"><i class="fas fa-ambulance"></i> Listado de Moviles</h3>
+
+<div class="row mb-4">
+    <div class="col-12 col-md-6">
+        <form method="GET" class="form-horizontal" action="">
+            <div class="input-group mb-sm-0">
+                <input class="form-control" type="text" name="search" autocomplete="off" id="for_search"
+                    style="text-transform: uppercase;"
+                    placeholder="Numero Movil" value="" required>
+                <div class="input-group-append">
+                    <button type="submit" class="btn btn-primary"><i class="fas fa-search"></i> Buscar</button>
+                </div>
+            </div>
+        </form>
+    </div>
+    <div class="col-12 col-md-2">
+        <a class="btn btn-success" href="{{ route('samu.mobile.create') }}">
+            <i class="fas fa-plus"></i> Agregar
+        </a>
+    </div>
+</div>
+<div class="table-responsive">
+    <table class="table table-sm table-bordered table-striped small">
+        <thead>
+            <tr class="text-center table-info">
+                <th>Número</th>
+                <th>Tipo</th>
+                <th>Estado</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>2</td>
+                <td>Movil 2</td>
+                <td class="bg-warning">Activo</td>
+                <td><a href="{{ route('samu.mobile.edit') }}">Editar</a> </td>
+            </tr>
+            <tr>
+                <td>4</td>
+                <td>Movil 12</td>
+                <td class="bg-warning">Activo</td>
+                <td><a href="{{ route('samu.mobile.edit') }}">Editar</a> </td>
+            </tr>
+            <tr>
+                <td>5</td>
+                <td>Movil 11</td>
+                <td class="bg-warning">Activo</td>
+                <td><a href="{{ route('samu.mobile.edit') }}">Editar</a> </td>
+            </tr>
+            <tr>
+                <td>8</td>
+                <td>Movil 15</td>
+                <td class="bg-warning">Activo</td>
+                <td><a href="{{ route('samu.mobile.edit') }}">Editar</a> </td>
+            </tr>
+
+            <tr>
+                <td>10</td>
+                <td>Movil 4</td>
+                <td class="bg-danger">Inactivo</td>
+                <td><a href="{{ route('samu.mobile.edit') }}">Editar</a> </td>
+            </tr>
+
+            <tr>
+                <td>11</td>
+                <td>Movil 13</td>
+                <td class="bg-danger">inactivo</td>
+                <td><a href="{{ route('samu.mobile.edit') }}">Editar</a> </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+@endsection
