fa58834b23edc32a132b6e04ba5f3b3c01a9a0d6
AdrianaOL <oviedolopezadriana13@gmail.com>
2021-09-21 09:07:48 -0300

    qtc_call_samu

--- app/Models/Samu/Qtc.php
+++ app/Models/Samu/Qtc.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Qtc extends Model
 {
     use HasFactory;
+    protected $table="samu_qtcs";
+
+    protected $fillable = [
+        
+        'class_qtc',
+        'hour',
+        'call_reception',
+        'telephone_information',
+        'applicant',
+        'direction',
+        'telephone',
+        'created_at'    
+
+    ];
+
+    public function follow()
+    {
+        return $this->hasOne(Follow::class, 'qtc_id');
+    }
 }
