90a87d0572c90891f95f78335c4e7e1de5298945
AdrianaOL <oviedolopezadriana13@gmail.com>
2021-10-06 11:41:18 -0300

    samu_permisos admin

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,35 +1,40 @@
 <ul class="nav nav-tabs mb-3">
 <li class="nav-item">
         <a class="nav-link
-        @if(request()->route()->view == 'pacientes') active @endif"
+        @if(request()->route()->view == 'home') active @endif"
+        href=" {{ route('samu.welcome') }}">Home</a>
+</li>
+<li class="nav-item">
+        <a class="nav-link
+        @if(request()->route()->view == 'shift') active @endif"
         href=" {{ route('samu.shift.index') }}">Turno</a>
 </li>
 <li class="nav-item">
         <a class="nav-link
-        @if(request()->route()->view == 'pacientes') active @endif"
+        @if(request()->route()->view == 'novelties') active @endif"
         href=" {{ route('samu.novelties.index') }}">Novedades</a>
 </li>
 <li class="nav-item">
         <a class="nav-link
-        @if(request()->route()->view == 'pacientes') active @endif"
+        @if(request()->route()->view == 'qtc') active @endif"
         href=" {{ route('samu.qtc.index') }}">Call Center</a>
     </li>
     
     <li class="nav-item">
         <a class="nav-link
-        @if(request()->route()->view == 'pacientes') active @endif"
+        @if(request()->route()->view == 'mobile') active @endif"
         href=" {{ route('samu.mobile.index') }}">Movil-Tripulacion</a>
     </li>
 
     <li class="nav-item">
         <a class="nav-link
-        @if(request()->route()->view == 'pacientes') active @endif"
+        @if(request()->route()->view == 'codekey') active @endif"
         href=" {{ route('samu.codekey.index') }}">Codificación de las Claves</a>
     </li>
     
     <li class="nav-item">
         <a class="nav-link
-        @if(request()->route()->view == 'pacientes') active @endif"
+        @if(request()->route()->view == 'codemobile') active @endif"
         href=" {{ route('samu.codemobile.index') }}">Codificación de Movil</a>
     </li>
 
