efa3d0f41058342ef7c344c32adad5eabee112a1
GermanZun <german.zuniga@gmail.com>
2021-11-19 13:00:41 -0300

    sic migration col order

--- database/migrations/2021_10_08_140626_create_sics_table.php
+++ database/migrations/2021_10_08_140626_create_sics_table.php
@@ -39,6 +39,11 @@ class CreateSicsTable extends Migration
             $table->string('patient_commune_code')->nullable();
             $table->integer('patient_phone_1')->nullable();
             $table->integer('patient_phone_2')->nullable();
+            $table->string('patient_social_name')->nullable();
+            $table->string('patient_email')->nullable();
+            $table->string('patient_nationality')->nullable();
+            $table->string('patient_ethnicity')->nullable();
+            $table->string('patient_marital_status')->nullable();
             $table->string('derivation_establishment_code')->nullable();
             $table->string('derivation_specialty_code')->nullable();
             $table->string('motive_indicator')->nullable();
@@ -68,12 +73,7 @@ class CreateSicsTable extends Migration
             $table->integer('fonasa_number')->nullable();
             $table->integer('certifying_institution_rut')->nullable();
             $table->integer('prais')->nullable();
-            $table->string('patient_social_name')->nullable();
             $table->string('tutor_name')->nullable();
-            $table->string('patient_email')->nullable();
-            $table->string('patient_nationality')->nullable();
-            $table->string('patient_ethnicity')->nullable();
-            $table->string('patient_marital_status')->nullable();
         
             
             $table->foreignId('status_id');
