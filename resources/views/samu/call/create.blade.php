c71adb1ef62eb8dc0a2705b2dd5a8ad62b2d17a5
rafael1delgado <delgadorafael2011@gmail.com>
2022-03-02 13:05:01 -0400

    [views] Agrega vista de las llamadas

--- resources/views/samu/call/create.blade.php
+++ resources/views/samu/call/create.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.app')
 
+@section('custom_js_head')
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin="" />
+
+<style>
+    #map { 
+    width: 100%;
+    height: 580px;
+}
+</style>
+@endsection
+
 @section('content')
 
 @include('samu.nav')
@@ -21,8 +32,45 @@
 
 </form>
 
+<div class="my-3">
+    <div id="map"></div>
+</div>
+ 
 @endsection
 
 @section('custom_js')
+<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
+<script>
+    const LATITUDE = -20.249956245793552;
+    const LONGITUDE = -70.12817358465354;
+    
+    let mapOptions = {
+        center: [LATITUDE, LONGITUDE],
+        zoom: 17
+    }
 
+    let map = new L.map('map' , mapOptions);
+    let layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+    map.addLayer(layer);
+
+    let marker = new L.Marker([LATITUDE, LONGITUDE], { 
+        draggable: true,
+    }).addTo(map);
+
+    marker.on('drag', (e) => {
+        var marker = e.target;
+        var latLang = marker.getLatLng();
+    
+        let setLatitude = latLang.lat;
+        let setLongitude = latLang.lng;
+        
+        let inputLatitude = document.getElementById("latitude");
+        let inputLongitude = document.getElementById("longitude");
+
+        inputLatitude.setAttribute('value', setLatitude.toFixed(8));
+        inputLongitude.setAttribute('value', setLongitude.toFixed(8));
+
+        marker.setLatLng(latLang);
+    });
+</script>
 @endsection
--- resources/views/samu/call/form.blade.php
+++ resources/views/samu/call/form.blade.php
@@ -1,60 +1,183 @@
 <div class="form-row">
 
-    <fieldset class="form-group col-md-1">
-        <label for="for_hour">Hora*</label>
-        <input type="time" class="form-control form-control-sm" name="hour" id="hour" value="{{ old('hour', ($call)? $call->hour->format('H:i') : date('H:i')) }}" readonly> 
-    </fieldset>
-
     <fieldset class="form-group col-md-3">
-        <label for="for_applicant">Solicitante </label>
-        <input type="text" class="form-control form-control-sm" name="applicant"
+        <label for="for-applicant">Solicitante</label>
+        <input type="text" class="form-control form-control-sm @error('applicant') is-invalid @enderror" name="applicant" id="for-applicant"
             value="{{ old('applicant', optional($call)->applicant) }}">
+        @error('applicant')
+            <div class="text-danger">
+                <small>{{ $message }}</small>
+            </div>
+        @enderror
     </fieldset>
 
     <fieldset class="form-group col-md-5">
-        <label for="for_address">Dirección </label>
-        <input type="text" class="form-control form-control-sm" name="address"
+        <label for="for-address">Dirección</label>
+        <input type="text" class="form-control form-control-sm @error('address') is-invalid @enderror" name="address" id="for-address"
             value="{{ old('address', optional($call)->address) }}">
+        @error('address')
+            <div class="text-danger">
+                <small>{{ $message }}</small>
+            </div>
+        @enderror
     </fieldset>
 
-    <fieldset class="form-group col-md-3">
-        <label for="for_telephone">Teléfono </label>
-        <input type="text" class="form-control form-control-sm" name="telephone"
+    <fieldset class="form-group col-md-2">
+        <label for="for-commune">Comuna</label>
+        <select class="form-control form-control-sm @error('commune_id') is-invalid @enderror" name="commune_id" id="for-commune">
+            <option value="">Selecciona una Comuna</option>
+            @foreach ($communes as $commune)
+                <option value="{{ $commune->id }}" {{ optional($call)->commune_id == $commune->id ? 'selected' : '' }}>
+                    {{ $commune->name }}
+                </option>
+            @endforeach
+        </select>
+        @error('commune_id')
+            <div class="text-danger">
+                <small>{{ $message }}</small>
+            </div>
+        @enderror
+    </fieldset>
+
+    <fieldset class="form-group col-md-2">
+        <label for="for-telephone">Teléfono</label>
+        <input type="text" class="form-control form-control-sm @error('telephone') is-invalid @enderror" name="telephone" id="for-telephone"
             value="{{ old('telephone', optional($call)->telephone) }}">
+        @error('telephone')
+            <div class="text-danger">
+                <small>{{ $message }}</small>
+            </div>
+        @enderror
     </fieldset>
-
 
 </div>
 
 <div class="form-row">
-    
+
     <fieldset class="form-group col-md-12">
-        <label for="for_information">Información telefónica*</label>
-        <textarea class="form-control form-control-sm" name="information" rows="5" required>{{ old('information', optional($call)->information) }}</textarea>
+        <label for="for-information">Información telefónica *</label>
+        <textarea class="form-control form-control-sm @error('information') is-invalid @enderror" name="information" rows="5" id="for-information" required>{{ old('information', optional($call)->information) }}</textarea>
+        @error('information')
+            <div class="text-danger">
+                <small>{{ $message }}</small>
+            </div>
+        @enderror
     </fieldset>
-    
+
 </div>
+
+<div class="form-row">
+    <fieldset class="form-group col-md-3">
+        <label for="for-age">Edad</label>
+        <input type="number" class="form-control form-control-sm @error('age') is-invalid @enderror" step=".1" name="age" id="for-age"
+            value="{{ old('age', optional($call)->age) }}">
+        @error('age')
+            <div class="text-danger">
+                <small>{{ $message }}</small>
+            </div>
+        @enderror
+    </fieldset>
+
+    <fieldset class="form-group col-md-3">
+        <label for="for-sex">Sexo</label>
+        <select class="form-control form-control-sm @error('sex') is-invalid @enderror" name="sex" id="for-sex">
+            <option value="">Selecciona un Sexo</option>
+            <option value="MALE" {{ optional($call)->sex == 'MALE' ? 'selected' : '' }}>Masculino</option>
+            <option value="FEMALE" {{ optional($call)->sex == 'FEMALE' ? 'selected' : '' }}>Femenino</option>
+            <option value="UNKNOWN" {{ optional($call)->sex == 'UNKNOWN' ? 'selected' : '' }}>Indeterminado</option>
+            <option value="OTHER" {{ optional($call)->sex == 'OTHER' ? 'selected' : '' }}>Otro</option>
+        </select>
+        @error('sex')
+            <div class="text-danger">
+                <small>{{ $message }}</small>
+            </div>
+        @enderror
+    </fieldset>
+
+    <fieldset class="form-group col-md-3">
+        <label for="for-reason">Motivo</label>
+        <input type="reason" class="form-control form-control-sm @error('reason') is-invalid @enderror" name="reason" id="for-reason"
+            value="{{ old('reason', optional($call)->reason) }}">
+        @error('reason')
+            <div class="text-danger">
+                <small>{{ $message }}</small>
+            </div>
+        @enderror
+    </fieldset>
+
+    <fieldset class="form-group col-md-3">
+        <label for="for-intervention">Intervención de Carabinero</label>
+        <select class="form-control form-control-sm @error('intervention') is-invalid @enderror" name="intervention" id="for-intervention">
+            <option value="">Selecciona una opción</option>
+            <option value="0" {{ optional($call)->intervention == '0' ? 'selected' : '' }}>No</option>
+            <option value="1" {{ optional($call)->intervention == '1' ? 'selected' : '' }}>Si</option>
+        </select>
+        @error('intervention')
+            <div class="text-danger">
+                <small>{{ $message }}</small> 
+            </div>
+        @enderror
+    </fieldset>
+
+</div>
+
+<div class="form-row">
+
+    <fieldset class="form-group col-md-3">
+        <label for="latitude">Latitud </label>
+        <input type="text" class="form-control form-control-sm @error('latitude') is-invalid @enderror" name="latitude" id="latitude"
+            value="{{ old('latitude', optional($call)->latitude) }}">
+        @error('latitude')
+            <div class="text-danger">
+                <small>{{ $message }}</small>
+            </div>
+        @enderror
+    </fieldset>
+
+    <fieldset class="form-group col-md-3">
+        <label for="longitude">Longitud</label>
+        <input type="text" class="form-control form-control-sm @error('longitude') is-invalid @enderror" name="longitude" id="longitude"
+            value="{{ old('longitude', optional($call)->longitude) }}">
+        @error('longitude')
+            <div class="text-danger">
+                <small>{{ $message }}</small>
+            </div>
+        @enderror
+    </fieldset>
+
+</div>
+
 
 @if(request()->routeIs('samu.call.edit'))
 
 <div class="form-row">
     <fieldset class="form-group col-md-3">
-        <label for="for_hour">Clasificación</label>
-        <select class="form-control form-control-sm" name="classification" id="classification" {{ optional($call)->classification == 'OT' ? 'disabled readonly' : '' }}>
-            <option></option>
+        <label for="for-classification">Clasificación</label>
+        <select class="form-control form-control-sm @error('classification') is-invalid @enderror" name="classification" id="for-classification" {{ optional($call)->classification == 'OT' ? 'disabled readonly' : '' }}>
+            <option value="">Selecciona una Clasificación</option>
             <option value="T1" {{ optional($call)->classification == 'T1' ? 'selected' : '' }}>T1</option>
             <option value="T2" {{ optional($call)->classification == 'T2' ? 'selected' : '' }}>T2</option>
             <option value="NM" {{ optional($call)->classification == 'NM' ? 'selected' : '' }}>NM</option>
             <option value="OT" {{ optional($call)->classification == 'OT' ? 'selected' : '' }}>OT</option>
         </select>
-        <small id="emailHelp" class="form-text text-danger">Si hace referencia a otra llamada, no debe clasificarla.</small>
+        <small id="for-classification" class="form-text text-danger">Si hace referencia a otra llamada, no debe clasificarla.</small>
+        @error('classification')
+            <div class="text-danger">
+                <small>{{ $message }}</small>
+            </div>
+        @enderror
     </fieldset>
 </div>
 <div class="form-row">
 
     <fieldset class="form-group col-md-12">
-        <label for="for_regulation">Regulación</label>
-        <textarea class="form-control form-control-sm" name="regulation" rows="5">{{ old('regulation', optional($call)->regulation) }}</textarea>
+        <label for="for-regulation">Regulación</label>
+        <textarea class="form-control form-control-sm @error('regulation') is-invalid @enderror" name="regulation" rows="5" id="for-regulation">{{ old('regulation', optional($call)->regulation) }}</textarea>
+        @error('regulation')
+            <div class="text-danger">
+                <small>{{ $message }}</small>
+            </div>
+        @enderror
     </fieldset>
 
 </div>
