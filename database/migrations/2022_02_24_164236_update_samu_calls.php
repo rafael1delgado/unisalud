35b8d7280cb2c48618b237d618bfc1940003cf94
rafael1delgado <delgadorafael2011@gmail.com>
2022-03-02 12:56:43 -0400

    [migrations] Agrega campos a samu_calls

---
+++ database/migrations/2022_02_24_164236_update_samu_calls.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateSamuCalls extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('samu_calls', function (Blueprint $table) {
+            $table->foreignId('commune_id')
+                ->nullable()
+                ->after('telephone')
+                ->constrained('communes');
+
+            $table->decimal('latitude', 10, 8)
+                ->after('address')
+                ->nullable();
+
+            $table->decimal('longitude', 10, 8)
+                ->after('latitude')
+                ->nullable();
+
+            $table->enum('sex', ['MALE', 'FEMALE', 'UNKNOWN', 'OTHER'])
+                ->after('regulation')
+                ->nullable();
+            
+            $table->string('reason') 
+                ->after('regulator_id')
+                ->nullable();
+
+            $table->boolean('intervention')
+                ->after('reason')
+                ->nullable();
+
+            $table->decimal('age') 
+                ->after('applicant')
+                ->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
