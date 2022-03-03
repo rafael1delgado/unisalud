bf690469f0bcaa002b0351b857725653f13382e4
rafael1delgado <delgadorafael2011@gmail.com>
2022-03-03 19:14:41 -0400

    [fix] Soluciona el nombre del archivo js

--- resources/views/samu/maps/maps.blade.php
+++ resources/views/samu/maps/maps.blade.php
@@ -23,5 +23,5 @@
 @section('custom_js')
 <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
 <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-<script src="{{ asset('/js/samu/call-maps.js') }}"></script>s
+<script src="{{ asset('/js/samu/call-maps.js') }}"></script>
 @endsection
