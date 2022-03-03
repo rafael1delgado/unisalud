5a7ab5a34639fde2f766689ed0a3983a39664893
rafael1delgado <delgadorafael2011@gmail.com>
2022-03-03 15:50:43 -0400

    [routes] Agrega ruta del gps

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\Parameters\RegionController;
 use App\Http\Controllers\Api\Samu\CallController;
+use App\Http\Controllers\Api\Samu\GpsController;
 use App\Http\Controllers\Api\Samu\MobileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -24,3 +25,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::get('calls', [CallController::class, 'index']);
 Route::get('mobiles', [MobileController::class, 'index']);
 Route::get('region/{region}/communes', [RegionController::class, 'communes']);
+Route::get('gps/mobile/{mobile}', [GpsController::class, 'index'])->middleware('auth.basic:,id');
