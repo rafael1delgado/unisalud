188fe21a89e58ddda0234725d872a1de81cc3d60
rafael1delgado <delgadorafael2011@gmail.com>
2022-03-03 15:49:58 -0400

    [controllers] Agrega controlador API del Gps

---
+++ app/Http/Controllers/Api/Samu/GpsController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Api\Samu;
+
+use App\Http\Controllers\Controller;
+use App\Models\Samu\Gps;
+use App\Models\Samu\Mobile;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class GpsController extends Controller
+{
+    public function index(Request $request, Mobile $mobile)
+    {
+        $data = $request->all();
+        if(isset($data['is_charging']) && isset($data['time']))
+        {
+            $data['is_charging'] = ($data['is_charging'] == 'false') ? false : true;
+            $data['time'] = Carbon::parse($data['time']);
+        }
+
+        $gps = Gps::create($data);
+        $gps->mobile()->associate($mobile);
+        $gps->save();
+
+        return $mobile;
+    }
+}
