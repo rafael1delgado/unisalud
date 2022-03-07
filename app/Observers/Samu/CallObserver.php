b76ce3e05403c45a3cf42ed4f1145938b83fbde2
rafael1delgado <delgadorafael2011@gmail.com>
2022-03-07 12:47:06 -0400

    [observer] Agrega CallObserver

---
+++ app/Observers/Samu/CallObserver.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Observers\Samu;
+
+use App\Models\Commune;
+use App\Models\Samu\Call;
+
+class CallObserver
+{
+    /**
+     * Handle the Call "creating" event.
+     *
+     * @param  \App\Models\Call  $call
+     * @return void
+     */
+    public function creating(Call $call)
+    {
+        $selectedCommune = Commune::find($call->commune_id);
+        
+        if($selectedCommune)
+        {
+            if ($selectedCommune->latitude == $call->latitude && $selectedCommune->longitude == $call->longitude)
+            {
+                $call->latitude = null;
+                $call->longitude = null;
+            }
+        }
+    }
+
+    /**
+     * Handle the Call "updating" event.
+     *
+     * @param  \App\Models\Call  $call
+     * @return void
+     */
+    public function updating(Call $call)
+    {
+        $selectedCommune = Commune::find($call->commune_id);
+        
+        if($selectedCommune)
+        {
+            if ($selectedCommune->latitude == $call->latitude && $selectedCommune->longitude == $call->longitude)
+            {
+                $call->latitude = null;
+                $call->longitude = null;
+            }
+        }
+    }
+
+}
