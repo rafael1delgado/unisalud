7d16633e2f7e3da4510c13cbb6694dc8706d1c45
rafael1delgado <delgadorafael2011@gmail.com>
2022-03-09 15:48:23 -0400

    [requests] Agrega unique para samu_mobiles en position y shift_id

--- app/Http/Requests/MobileInService/MobileInServiceStoreRequest.php
+++ app/Http/Requests/MobileInService/MobileInServiceStoreRequest.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Requests\MobileInService;
 
+use App\Models\Samu\Shift;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
 class MobileInServiceStoreRequest extends FormRequest
 {
+    public function __construct()
+    {
+        $this->shift = Shift::whereStatus(true)->first();
+    }
+    
     /**
      * Determine if the user is authorized to make this request.
      *
@@ -27,7 +33,8 @@ class MobileInServiceStoreRequest extends FormRequest
         return [
             'position'      => [
                 'required',
-                Rule::unique('samu_mobiles_in_service')
+                Rule::unique('samu_mobiles_in_service', 'position')
+                    ->where('shift_id', $this->shift->id)
             ],
             'mobile_id'     => 'required|exists:samu_mobiles,id',
             'type'          => 'required|string',
--- app/Http/Requests/MobileInService/MobileInServiceUpdateRequest.php
+++ app/Http/Requests/MobileInService/MobileInServiceUpdateRequest.php
@@ -3,11 +3,17 @@
 namespace App\Http\Requests\MobileInService;
 
 use App\Models\Samu\MobileInService;
+use App\Models\Samu\Shift;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
 class MobileInServiceUpdateRequest extends FormRequest
 {
+    public function __construct()
+    {
+        $this->shift = Shift::whereStatus(true)->first();
+    }
+    
     /**
      * Determine if the user is authorized to make this request.
      *
@@ -28,7 +34,9 @@ class MobileInServiceUpdateRequest extends FormRequest
         return [
             'position'      => [
                 'required',
-                Rule::unique('samu_mobiles_in_service')->ignore($this->route('mobileInService')->id)
+                Rule::unique('samu_mobiles_in_service', 'position')
+                    ->where('shift_id',$this->shift->id)
+                    ->ignore($this->route('mobileInService')->id)
             ],
             'mobile_id'     => 'required|exists:samu_mobiles,id',
             'type'          => 'required|string',
