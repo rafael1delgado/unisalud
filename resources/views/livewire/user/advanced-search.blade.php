70cf23f6267cf07f618b2b9facdbe84e3da5508a
GermanZun <german.zuniga@gmail.com>
2022-03-14 13:05:50 -0300

    edit icon index user

--- resources/views/livewire/user/advanced-search.blade.php
+++ resources/views/livewire/user/advanced-search.blade.php
@@ -46,16 +46,16 @@
                         <td>{{($patient) ? $patient->officialPhone : ''}}</td>
                         <td>{{($patient && $patient->officialEmail) ? $patient->officialEmail : ''}}</td>
                         <td nowrap>
-                            <a class="btn-primary btn-sm mr-1" title="Seleccionar" href="{{ route('user.edit',$patient->id)}}"><span class="fas fa-check" aria-hidden="true"></span></a>
+                            <a class="btn-primary btn-sm mr-1" title="Editar" href="{{ route('user.edit',$patient->id)}}"><span class="fas fa-edit" aria-hidden="true"></span></a>
                             @can('be god')
-                                <a class="btn-warning btn-sm" href="{{ route('user.switch', $patient->id)}}"><span class="fas fa-redo" aria-hidden="true"></span></a>
+                                <a class="btn-warning btn-sm" title="Cambiar a usuario" href="{{ route('user.switch', $patient->id)}}"><span class="fas fa-redo" aria-hidden="true"></span></a>
                             @endcan
                         </td>
                     </tr>
                     @empty
                         <tr><th scope="row" colspan="8" class="text-center">No hay coincidencias con la búsqueda <a class="btn-primary btn-sm" href="{{ route('user.create')}}"> Ingresar uno nuevo</a></td></th>
                     @endforelse
-                    @if($patients->count() > 0) <tr><th scope="row" colspan="8" class="text-center">Si ninguno en la búsqueda corresponde al usuario que estas buscando <a class="btn-primary btn-sm" href="{{ route('user.create')}}"> Ingresar uno nuevo</a></td></th> @endif
+                    @if($patients->count() > 0) <tr><th scope="row" colspan="8" class="text-center">Si ninguno en la búsqueda corresponde al usuario que estas buscando <a class="btn-primary btn-sm" href="{{ route('user.create')}}"> <i class="fas fa-user-plus"></i> Ingresar uno nuevo</a></td></th> @endif
                 @endif
             </tbody>
         </table>
