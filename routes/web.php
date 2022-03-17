99647981a666385c46372047019b1233d2013e20
rafael1delgado <delgadorafael2011@gmail.com>
2022-03-17 18:07:18 -0400

    [routes] Agrega route para guardar evento duplicado. Actualiza ruta para duplicar evento

--- routes/web.php
+++ routes/web.php
@@ -548,9 +548,10 @@ Route::prefix('samu')->name('samu.')->middleware('auth')->group(function () {
 	->middleware('permission:SAMU administrador|SAMU despachador')
 	->group(function () {
 		Route::get('/', 			[EventController::class, 'index'])->name('index');
-		Route::get('/create/duplicate/{event}', [EventController::class, 'create'])->name('duplicate');
+		Route::get('/{event}/duplicate', [EventController::class, 'create'])->name('duplicate');
 		Route::get('/create/{call?}', [EventController::class, 'create'])->name('create');
-		Route::post('/store',		[EventController::class, 'store'])->name('store');
+		Route::post('/store/{call?}', [EventController::class, 'store'])->name('store');
+		Route::post('/store/{event?}/duplicate', [EventController::class, 'store'])->name('store.duplicate');
 		Route::get('/edit/{event}', [EventController::class, 'edit'])->name('edit');
 		Route::put('/update/{event}',[EventController::class, 'update'])->name('update');
 		Route::delete('/{event}', 	[EventController::class, 'destroy'])->name('destroy');
