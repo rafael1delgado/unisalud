f62799c5dd4ed1b8861e91537f938c203d7a3855
rafael1delgado <delgadorafael2011@gmail.com>
2022-03-22 13:46:31 -0400

    [views] Agrega clases para mostrar error de validaciones

--- resources/views/livewire/samu/vital-sign/vital-sign-create.blade.php
+++ resources/views/livewire/samu/vital-sign/vital-sign-create.blade.php
@@ -33,7 +33,7 @@
                     <td>{{ $vs['fill_capillary'] ? $vs['fill_capillary'] : '-'}}</td>
                     <td>{{ $vs['t'] ? $vs['t'] : '-'}}</td>
                     <td>
-                        <button type="button" class="btn btn-sm btn-danger" wire:click="deleteVitalSign({{ $index }})">
+                        <button type="button" class="btn btn-sm btn-danger" title="Elimina Signo Vital" wire:click="deleteVitalSign({{ $index }})">
                             <i class="fas fa-trash"></i>
                         </button>
                     </td>
@@ -52,53 +52,108 @@
     <div class="form-row">
         <fieldset class="form-group col-6 col-md-1">
             <label for="for-fc">Frecuencia <br>Cardiaca</label>
-            <input type="text" class="form-control" maxlength="8" wire:model="fc" id="for-fc" value="{{ old('fc', optional($event)->fc) }}">
+            <input type="text" class="form-control @error('fc') is-invalid @enderror" maxlength="8" wire:model="fc" id="for-fc" value="{{ old('fc', optional($event)->fc) }}">
+            @error('fc')
+                <div class="text-danger">
+                    <small>{{ $message }}</small>
+                </div>
+            @enderror
         </fieldset>
         <fieldset class="form-group col-6 col-md-1">
             <label for="for-fr">Frecuencia <br>Respiratoria</label>
-            <input type="number" class="form-control" wire:model="fr" id="for-fr" value="{{ old('fr', optional($event)->fr) }}">
+            <input type="number" class="form-control @error('fr') is-invalid @enderror" wire:model="fr" id="for-fr" value="{{ old('fr', optional($event)->fr) }}">
+            @error('fr')
+                <div class="text-danger">
+                    <small>{{ $message }}</small>
+                </div>
+            @enderror
         </fieldset>
         <fieldset class="form-group col-6 col-md-1">
             <label for="for-pa">Presión <br>Arterial</label>
-            <input type="text" class="form-control" wire:model="pa" id="for-pa" value="{{ old('pa', optional($event)->pa) }}" 
+            <input type="text" class="form-control @error('pa') is-invalid @enderror" wire:model="pa" id="for-pa" value="{{ old('pa', optional($event)->pa) }}" 
                 placeholder="xxx/xx">
+            @error('pa')
+                <div class="text-danger">
+                    <small>{{ $message }}</small>
+                </div>
+            @enderror
         </fieldset>
         <fieldset class="form-group col-6 col-md-1">
             <label for="for-pam">Presión Arterial <br>Media</label>
-            <input type="text" class="form-control" wire:model="pam" id="for-pam" value="{{ old('pam', optional($event)->pam) }}"
+            <input type="text" class="form-control @error('pam') is-invalid @enderror" wire:model="pam" id="for-pam" value="{{ old('pam', optional($event)->pam) }}"
                 placeholder="xxx/xx">
+            @error('pam')
+                <div class="text-danger">
+                    <small>{{ $message }}</small>
+                </div>
+            @enderror
         </fieldset>
         <fieldset class="form-group col-6 col-md-1">
             <label for="for-gl">Glasgow<br>&nbsp;</label>
-            <input type="number" class="form-control" wire:model="gl" id="for-gl" value="{{ old('gl', optional($event)->gl) }}">
+            <input type="number" class="form-control @error('gl') is-invalid @enderror" wire:model="gl" id="for-gl" value="{{ old('gl', optional($event)->gl) }}">
+            @error('gl')
+                <div class="text-danger">
+                    <small>{{ $message }}</small>
+                </div>
+            @enderror
         </fieldset>
         <fieldset class="form-group col-6 col-md-1">
             <label for="for-soam">% Saturacion <br>Oxigeno/Ambi.</label>
-            <input type="number" class="form-control" wire:model="soam" id="for-soam" value="{{ old('soam', optional($event)->soam) }}">
+            <input type="number" class="form-control @error('soam') is-invalid @enderror" wire:model="soam" id="for-soam" value="{{ old('soam', optional($event)->soam) }}">
+            @error('soam')
+                <div class="text-danger">
+                    <small>{{ $message }}</small>
+                </div>
+            @enderror
         </fieldset>
         <fieldset class="form-group col-6 col-md-1">
             <label for="for-soap">% Saturación <br>Oxigeno/Apoyo</label>
-            <input type="number" class="form-control" wire:model="soap" id="for-soap" value="{{ old('soap', optional($event)->soap) }}">
+            <input type="number" class="form-control @error('soap') is-invalid @enderror" wire:model="soap" id="for-soap" value="{{ old('soap', optional($event)->soap) }}">
+            @error('soap')
+                <div class="text-danger">
+                    <small>{{ $message }}</small>
+                </div>
+            @enderror
         </fieldset>
         <fieldset class="form-group col-6 col-md-1">
             <label for="for-hgt">HGT <br>mg/dl</label>
-            <input type="number" class="form-control" wire:model="hgt" id="for-hgt" value="{{ old('hgt', optional($event)->hgt) }}">
+            <input type="number" class="form-control @error('hgt') is-invalid @enderror" wire:model="hgt" id="for-hgt" value="{{ old('hgt', optional($event)->hgt) }}">
+            @error('hgt')
+                <div class="text-danger">
+                    <small>{{ $message }}</small>
+                </div>
+            @enderror
         </fieldset>
         <fieldset class="form-group col-6 col-md-1">
             <label for="for-fill-capillary">Llene <br>Capilar</label>
-            <input type="number" class="form-control" wire:model="fill_capillary" id="for-fill-capillary" value="{{ old('fill_capillary', optional($event)->fill_capillary) }}">
+            <input type="number" class="form-control @error('fill_capillary') is-invalid @enderror" wire:model="fill_capillary" id="for-fill-capillary" value="{{ old('fill_capillary', optional($event)->fill_capillary) }}">
+            @error('fill_capillary')
+                <div class="text-danger">
+                    <small>{{ $message }}</small>
+                </div>
+            @enderror
         </fieldset>
         <fieldset class="form-group col-6 col-md-1">
             <label for="for-t">Temperatura <br>°C</label>
-            <input type="number" class="form-control" step=".01" wire:model="t" id="for-t" value="{{ old('t', optional($event)->t) }}">
+            <input type="number" class="form-control @error('t') is-invalid @enderror" step=".01" wire:model="t" id="for-t" value="{{ old('t', optional($event)->t) }}">
+            @error('t')
+                <div class="text-danger">
+                    <small>{{ $message }}</small>
+                </div>
+            @enderror
         </fieldset>
         <fieldset class="form-group col-6 col-md-1">
-            <label for="for-created-at">Fecha Muestra</label>
-            <input type="datetime-local" class="form-control" id="for-created-at" wire:model="datetime">
+            <label for="for-created-at">Fecha y Hora</label>
+            <input type="datetime-local" class="form-control @error('datetime') is-invalid @enderror" id="for-created-at" wire:model="datetime">
+            @error('datetime')
+                <div class="text-danger">
+                    <small>{{ $message }}</small>
+                </div>
+            @enderror
         </fieldset>
         <fieldset class="form-group col-6 col-md-1 mt-5 pt-1">
-            <button type="button" id="btn-add" wire:click="addVitalSign()" class="btn btn-block btn-sm btn-primary">
-                Agregar
+            <button type="button" id="btn-add" wire:click="addVitalSign()" class="btn btn-block btn-sm btn-success">
+                <i class="fas fa-plus"></i> Agregar
             </button>
         </fieldset>
         
