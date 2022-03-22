cee4a875736be2549629b41bc5ddb09cf41f4406
rafael1delgado <delgadorafael2011@gmail.com>
2022-03-22 13:02:00 -0400

    [migrations] Agrega tabla samu_vital_signs

---
+++ database/migrations/2022_03_21_112742_samu_vital_signs.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class SamuVitalSigns extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('samu_vital_signs', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('fc', 10)->nullable();
+            $table->integer('fr')->nullable();
+            $table->string('pa')->nullable();
+            $table->string('pam')->nullable();
+            $table->integer('gl')->nullable();
+            $table->integer('soam')->nullable();
+            $table->integer('soap')->nullable();
+            $table->integer('hgt')->nullable();
+            $table->integer('fill_capillary')->nullable();
+            $table->decimal('t',5,2)->nullable();
+            $table->timestamp('datetime')->nullable();
+
+            $table->foreignId('event_id')->nullable()->constrained('samu_events');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('samu_vital_signs');
+    }
+}
