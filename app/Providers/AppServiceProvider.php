8d513a6787064b7eb44dda994bb2a6d9f8dc8a30
rafael1delgado <delgadorafael2011@gmail.com>
2022-03-23 19:49:07 -0400

    [providers] Registra los charts en el boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,16 @@
 
 namespace App\Providers;
 
+use App\Charts\Samu\EventByCommune;
+use App\Charts\Samu\EventByMobile;
+use App\Charts\Samu\EventByMonth;
+use App\Charts\Samu\EventBySex;
+use App\Charts\Samu\EventLastMonth;
+use App\Charts\Samu\SampleChart;
+
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use ConsoleTVs\Charts\Registrar as Charts;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,8 +30,17 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(Charts $charts)
     {
         Paginator::useBootstrap();
+        $charts->register([
+            SampleChart::class,
+            EventByCommune::class,
+            EventByMonth::class,
+            EventByMobile::class,
+            EventBySex::class,
+            EventLastMonth::class
+        ]);
+
     }
 }
