5b1809d013456674685f223a2fa6f8075c2bde5c
rafael1delgado <delgadorafael2011@gmail.com>
2022-03-25 14:51:07 -0400

    [views] Actualiza viesta del componente que gestiona los signos vitales

--- resources/views/livewire/samu/vital-sign/vital-sign-create.blade.php
+++ resources/views/livewire/samu/vital-sign/vital-sign-create.blade.php
@@ -2,7 +2,7 @@
     <div class="form-row">
         <fieldset class="form-group col-6 col-md-1">
             <label for="for-registered-at">Hora<br>&nbsp;</label>
-            <input type="time" class="form-control @error('registered_at') is-invalid @enderror" id="for-registered-at" wire:model="registered_at">
+            <input @if($edit) wire:model="registered_at" @else name="registered_at" @endif type="time" class="form-control @error('registered_at') is-invalid @enderror" id="for-registered-at">
             @error('registered_at')
                 <div class="text-danger">
                     <small>{{ $message }}</small>
@@ -11,7 +11,7 @@
         </fieldset>
         <fieldset class="form-group col-6 col-md-1">
             <label for="for-fc">Frecuencia <br>Cardiaca</label>
-            <input type="text" class="form-control @error('fc') is-invalid @enderror" maxlength="8" wire:model="fc" id="for-fc" value="{{ old('fc', optional($event)->fc) }}">
+            <input @if($edit) wire:model="fc" @else name="fc" @endif type="text" class="form-control @error('fc') is-invalid @enderror" maxlength="8" id="for-fc" value="{{ old('fc', optional($event)->fc) }}">
             @error('fc')
                 <div class="text-danger">
                     <small>{{ $message }}</small>
@@ -20,7 +20,7 @@
         </fieldset>
         <fieldset class="form-group col-6 col-md-1">
             <label for="for-fr">Frecuencia <br>Respiratoria</label>
-            <input type="number" class="form-control @error('fr') is-invalid @enderror" wire:model="fr" id="for-fr" value="{{ old('fr', optional($event)->fr) }}">
+            <input @if($edit) wire:model="fr" @else name="fr" @endif type="number" class="form-control @error('fr') is-invalid @enderror" id="for-fr" value="{{ old('fr', optional($event)->fr) }}">
             @error('fr')
                 <div class="text-danger">
                     <small>{{ $message }}</small>
@@ -29,7 +29,7 @@
         </fieldset>
         <fieldset class="form-group col-6 col-md-1">
             <label for="for-pa">Presión <br>Arterial</label>
-            <input type="text" class="form-control @error('pa') is-invalid @enderror" wire:model="pa" id="for-pa" value="{{ old('pa', optional($event)->pa) }}" 
+            <input @if($edit) wire:model="pa" @else name="pa" @endif type="text" class="form-control @error('pa') is-invalid @enderror" id="for-pa" value="{{ old('pa', optional($event)->pa) }}" 
                 placeholder="xxx/xx">
             @error('pa')
                 <div class="text-danger">
@@ -39,7 +39,7 @@
         </fieldset>
         <fieldset class="form-group col-6 col-md-1">
             <label for="for-pam">Presión Arterial <br>Media</label>
-            <input type="text" class="form-control @error('pam') is-invalid @enderror" wire:model="pam" id="for-pam" value="{{ old('pam', optional($event)->pam) }}"
+            <input @if($edit) wire:model="pam" @else name="pam" @endif type="text" class="form-control @error('pam') is-invalid @enderror" id="for-pam" value="{{ old('pam', optional($event)->pam) }}"
                 placeholder="xxx/xx">
             @error('pam')
                 <div class="text-danger">
@@ -49,7 +49,7 @@
         </fieldset>
         <fieldset class="form-group col-6 col-md-1">
             <label for="for-gl">Glasgow<br>&nbsp;</label>
-            <input type="number" class="form-control @error('gl') is-invalid @enderror" wire:model="gl" id="for-gl" value="{{ old('gl', optional($event)->gl) }}">
+            <input @if($edit) wire:model="gl" @else name="gl" @endif type="number" class="form-control @error('gl') is-invalid @enderror" id="for-gl" value="{{ old('gl', optional($event)->gl) }}">
             @error('gl')
                 <div class="text-danger">
                     <small>{{ $message }}</small>
@@ -58,7 +58,7 @@
         </fieldset>
         <fieldset class="form-group col-6 col-md-1">
             <label for="for-soam">% Saturacion <br>Oxigeno/Ambi.</label>
-            <input type="number" class="form-control @error('soam') is-invalid @enderror" wire:model="soam" id="for-soam" value="{{ old('soam', optional($event)->soam) }}">
+            <input @if($edit) wire:model="soam" @else name="soam" @endif type="number" class="form-control @error('soam') is-invalid @enderror" id="for-soam" value="{{ old('soam', optional($event)->soam) }}">
             @error('soam')
                 <div class="text-danger">
                     <small>{{ $message }}</small>
@@ -67,7 +67,7 @@
         </fieldset>
         <fieldset class="form-group col-6 col-md-1">
             <label for="for-soap">% Saturación <br>Oxigeno/Apoyo</label>
-            <input type="number" class="form-control @error('soap') is-invalid @enderror" wire:model="soap" id="for-soap" value="{{ old('soap', optional($event)->soap) }}">
+            <input @if($edit) wire:model="soap" @else name="soap" @endif type="number" class="form-control @error('soap') is-invalid @enderror" id="for-soap" value="{{ old('soap', optional($event)->soap) }}">
             @error('soap')
                 <div class="text-danger">
                     <small>{{ $message }}</small>
@@ -76,7 +76,7 @@
         </fieldset>
         <fieldset class="form-group col-6 col-md-1">
             <label for="for-hgt">HGT <br>mg/dl</label>
-            <input type="number" class="form-control @error('hgt') is-invalid @enderror" wire:model="hgt" id="for-hgt" value="{{ old('hgt', optional($event)->hgt) }}">
+            <input @if($edit) wire:model="hgt" @else name="hgt" @endif type="number" class="form-control @error('hgt') is-invalid @enderror" id="for-hgt" value="{{ old('hgt', optional($event)->hgt) }}">
             @error('hgt')
                 <div class="text-danger">
                     <small>{{ $message }}</small>
@@ -85,7 +85,7 @@
         </fieldset>
         <fieldset class="form-group col-6 col-md-1">
             <label for="for-fill-capillary">Llene <br>Capilar</label>
-            <input type="number" class="form-control @error('fill_capillary') is-invalid @enderror" wire:model="fill_capillary" id="for-fill-capillary" value="{{ old('fill_capillary', optional($event)->fill_capillary) }}">
+            <input @if($edit) wire:model="fill_capillary" @else name="fill_capillary" @endif type="number" class="form-control @error('fill_capillary') is-invalid @enderror" id="for-fill-capillary" value="{{ old('fill_capillary', optional($event)->fill_capillary) }}">
             @error('fill_capillary')
                 <div class="text-danger">
                     <small>{{ $message }}</small>
@@ -94,23 +94,37 @@
         </fieldset>
         <fieldset class="form-group col-6 col-md-1">
             <label for="for-t">Temperatura <br>°C</label>
-            <input type="number" class="form-control @error('t') is-invalid @enderror" step=".01" wire:model="t" id="for-t" value="{{ old('t', optional($event)->t) }}">
+            <input @if($edit) wire:model="t" @else name="t" @endif type="number" class="form-control @error('t') is-invalid @enderror" step=".01" id="for-t" value="{{ old('t', optional($event)->t) }}">
             @error('t')
                 <div class="text-danger">
                     <small>{{ $message }}</small>
                 </div>
             @enderror
         </fieldset>
+        @if($edit && $event->status)
         <fieldset class="form-group col-6 col-md-1 mt-5">
-            <button type="button" id="btn-add" wire:click="addVitalSign()" class="btn btn-block btn-success">
-                <i class="fas fa-plus"></i> Agregar
+            <button 
+                class="btn btn-block btn-success"
+                type="button"
+                wire:click="updateVitalSign()"
+                wire:loading.attr="disabled"
+                wire:target="updateVitalSign"
+                >
+                <span
+                    class="spinner-border spinner-border-sm"
+                    role="status"
+                    aria-hidden="true"
+                    wire:loading
+                    wire:target="updateVitalSign">
+                </span>
+                <i class="fas fa-sync" wire:loading.remove wire:target="updateVitalSign"></i> 
+                Actualizar
             </button>
         </fieldset>
-        
+        @endif
     </div>
 
-    @if($vitalSigns->isNotEmpty())
-    <div class="table-responsive">
+    {{-- <div class="table-responsive">
         <table class="table table-sm table-bordered">
             <thead>
                 <tr>
@@ -129,29 +143,25 @@
                 </tr>
             </thead>
             <tbody>
-                <input type="hidden" name="vital_signs" value="{{ $vitalSigns }}">                    
-                @foreach($vitalSigns as $index => $vs)
                 <tr class="text-center">
-                    <td>{{ $vs['registered_at'] ? $vs['registered_at'] : '-' }}</td>
-                    <td>{{ $vs['fc'] ? $vs['fc'] : '-' }}</td>
-                    <td>{{ $vs['fr'] ? $vs['fr'] : '-' }}</td>
-                    <td>{{ $vs['pa'] ? $vs['pa'] : '-' }}</td>
-                    <td>{{ $vs['pam'] ? $vs['pam'] : '-'}}</td>
-                    <td>{{ $vs['gl'] ? $vs['gl'] : '-' }}</td>
-                    <td>{{ $vs['soam'] ? $vs['soam'] : '-'}}</td>
-                    <td>{{ $vs['soap'] ? $vs['soap'] : '-'}}</td>
-                    <td>{{ $vs['hgt'] ? $vs['hgt'] : '-'}}</td>
-                    <td>{{ $vs['fill_capillary'] ? $vs['fill_capillary'] : '-'}}</td>
-                    <td>{{ $vs['t'] ? $vs['t'] : '-'}}</td>
+                    <td>{{ $event->vitalSign->registered_at ? $event->vitalSign->registered_at : '-' }}</td>
+                    <td>{{ $event->vitalSign->fc ? $event->vitalSign->fc : '-' }}</td>
+                    <td>{{ $event->vitalSign->fr ? $event->vitalSign->fr : '-' }}</td>
+                    <td>{{ $event->vitalSign->pa ? $event->vitalSign->pa : '-' }}</td>
+                    <td>{{ $event->vitalSign->pam ? $event->vitalSign->pam : '-'}}</td>
+                    <td>{{ $event->vitalSign->gl ? $event->vitalSign->gl : '-' }}</td>
+                    <td>{{ $event->vitalSign->soam ? $event->vitalSign->soam : '-'}}</td>
+                    <td>{{ $event->vitalSign->soap ? $event->vitalSign->soap : '-'}}</td>
+                    <td>{{ $event->vitalSign->hgt ? $event->vitalSign->hgt : '-'}}</td>
+                    <td>{{ $event->vitalSign->fill_capillary ? $event->vitalSign->fill_capillary : '-'}}</td>
+                    <td>{{ $event->vitalSign->t ? $event->vitalSign->t : '-'}}</td>
                     <td>
-                        <button type="button" class="btn btn-sm btn-danger" title="Elimina Signo Vital" wire:click="deleteVitalSign({{ $index }})">
+                        <button type="button" class="btn btn-sm btn-danger" title="Elimina Signo Vital">
                             <i class="fas fa-trash"></i>
                         </button>
                     </td>
                 </tr>
-                @endforeach
             </tbody>
         </table>
-    </div>
-    @endif
+    </div> --}}
 </div>
