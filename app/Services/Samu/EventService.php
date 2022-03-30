3ceeade1d5fdf343ead43df19c9be334ce126a6e
rafael1delgado <delgadorafael2011@gmail.com>
2022-03-30 13:37:05 -0400

    [services] Agrega descripciones de las funciones y ajusta logica para guardar eventos

--- app/Services/Samu/EventService.php
+++ app/Services/Samu/EventService.php
@@ -29,9 +29,12 @@ class EventService
         $newEvent->call()->associate($callRelationed);
         $newEvent->save();
 
-        $vitalSign = VitalSign::create($this->dataVitalSign);
-        $newEvent->vitalSign()->save($vitalSign);
-        $newEvent->save();
+        if($this->notEmptyVitalSign($dataValidated))
+        {
+            $vitalSign = VitalSign::create($this->dataVitalSign);
+            $newEvent->vitalSign()->save($vitalSign);
+            $newEvent->save();
+        }
     }
 
     /**
@@ -67,10 +70,16 @@ class EventService
         }
     }
 
-
+    /**
+     * Get field of event and vital sign from dataValidated
+     * 
+     * @param  array  $dataValidated
+     * @param  \App\Models\Samu\Event  $event
+     * 
+     * @return void
+     */
     public function getDataVitalSign($dataValidated, Event $event = null)
     {
-        
         $this->dataVitalSign['fc'] = $dataValidated['fc'];
         $this->dataVitalSign['fr'] = $dataValidated['fr'];
         $this->dataVitalSign['pa'] = $dataValidated['pa'];
@@ -103,11 +112,39 @@ class EventService
         $this->dataEvent = $dataValidated;
     }
 
+    /**
+     * Get the date for the registered_at 
+     * 
+     * @param  \App\Models\Samu\Event  $event
+     * @return string
+     */
     public function getDate(Event $event = null)
     {
         $date = now()->format('Y-m-d ');
         if($event && $event->vitalSign && $event->vitalSign->registered_at)
             $date = $event->vitalSign->registered_at->format('Y-m-d ');
         return $date;
+    }
+
+    /**
+     * checks if at least one data of the vital signs are defined
+     * 
+     * @param  array  $dataValidated
+     * @return boolean
+     */
+    public function notEmptyVitalSign($dataValidated)
+    {
+        return (
+            $dataValidated['fc'] ||
+            $dataValidated['fr'] ||
+            $dataValidated['pa'] ||
+            $dataValidated['pam'] ||
+            $dataValidated['gl'] ||
+            $dataValidated['soam'] ||
+            $dataValidated['soap'] ||
+            $dataValidated['hgt'] ||
+            $dataValidated['fill_capillary'] ||
+            $dataValidated['t']
+        );
     }
 }
