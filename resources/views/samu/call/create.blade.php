f2e970b341cf8ffa8174690865200575a19ec498
rafael1delgado <delgadorafael2011@gmail.com>
2022-04-04 11:07:25 -0400

    [views] Agrega include para mensajes y actualiza los scripts

--- resources/views/samu/call/create.blade.php
+++ resources/views/samu/call/create.blade.php
@@ -24,8 +24,7 @@
     @csrf
     @method('POST')
 
-    <div id="danger-msg"  class='alert alert-danger' style="display:none"></div>
-    <div id="success-msg"  class='alert alert-success' style="display:none"></div>
+    @include('samu.call.partials.message-for-script')
 
     @include('samu.call.form', ['call' => null])
 
@@ -34,70 +33,11 @@
 @endsection
 
 @section('custom_js')
+<script>
+    const API_KEY = '{{ env("API_KEY_HERE") }}';
+</script>
 <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
 <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 <script src="{{ asset('/js/samu/call-form.js') }}"></script>
-<script>
-    const URL_GEOCODE = 'https://geocode.search.hereapi.com/v1/geocode';
-    const API_KEY = '{{ env("API_KEY_HERE") }}';
-    const ZOOM_SEARCH = 15;
-    let btnSearch = document.getElementById('btn-search');
-
-    function showError(msg) {
-        hiddenSuccess();
-        document.getElementById("danger-msg").innerHTML = msg;
-        document.getElementById("danger-msg").style.display = 'block';
-    }
-
-    function showSuccess(msg) {
-        hiddenError();
-        document.getElementById("success-msg").innerHTML = msg;
-        document.getElementById("success-msg").style.display = 'block';
-    }
-
-    function hiddenError() {
-        document.getElementById("danger-msg").style.display = 'none';
-    }
-
-    function hiddenSuccess() {
-        document.getElementById("success-msg").style.display = 'none';
-    }
-
-    btnSearch.addEventListener('click', (event) => {
-        let address = document.getElementById('for-address');
-        let commune = document.getElementById('for-commune');
-        let country = 'chile';
-        commune = (commune.value != '') ? commune.options[commune.selectedIndex].text : '';
-
-        if(address.value != '') {
-            getLocation(address.value, commune, country);
-        } else {
-            showError('El campo dirección es obligatorio para autoposicionar el pin en el mapa.');
-        }
-    });
-
-    async function getLocation(address, commune, country) {
-        try {
-            const params = {
-                q: `${address} ${commune} ${country}`,
-                apiKey: API_KEY
-            };
-            const response = await axios.get(`${URL_GEOCODE}`, { params });
-            let locations = response.data.items;
-
-            if(locations.length != 0) {
-                let location = locations[0].position;
-                marker.setLatLng([location.lat, location.lng]);
-                map.setView([location.lat, location.lng], ZOOM_SEARCH);
-                inputLatitude.setAttribute('value', location.lat);
-                inputLongitude.setAttribute('value', location.lng);
-                showSuccess('El pin fue autoposicionado.');
-            } else {
-                showError('La dirección indicada no fue encontrada.');
-            }
-        } catch (error) {
-            showError('Disculpe, en este momento no podemos buscar la dirección indicada.');
-        }
-    }
-</script>
+<script src="{{ asset('/js/samu/call-search-location.js') }}"></script>
 @endsection
--- resources/views/samu/call/edit.blade.php
+++ resources/views/samu/call/edit.blade.php
@@ -4,7 +4,7 @@
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin="" />
 
 <style>
-    #map { 
+    #map {
         width: 100%;
         height: 400px;
     }
@@ -17,10 +17,12 @@
 
 <h3 class="mb-3"><i class="fas fa-clipboard-check"></i> Datos de la llamada ID: {{ $call->id }}</h3>
 
-<!-- Edit --> 
+<!-- Edit -->
 <form method="POST" action="{{ route('samu.call.update', $call) }}">
     @csrf
     @method('PUT')
+
+    @include('samu.call.partials.message-for-script')
 
     @include('samu.call.form', ['call' => $call])
 
@@ -59,7 +61,11 @@
 @endsection
 
 @section('custom_js')
+<script>
+    const API_KEY = '{{ env("API_KEY_HERE") }}';
+</script>
 <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
 <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 <script src="{{ asset('/js/samu/call-form.js') }}"></script>
+<script src="{{ asset('/js/samu/call-search-location.js') }}"></script>
 @endsection
