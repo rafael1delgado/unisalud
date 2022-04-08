4a0748d9c0845eb6f2eac811d12300463d6c6864
GermanZun <german.zuniga@gmail.com>
2022-04-08 10:43:32 -0400

    oauth2 auth config

--- database/migrations/2022_03_31_094049_add_address_reference_to_samu_calls.php
+++ database/migrations/2022_03_31_094049_add_address_reference_to_samu_calls.php
@@ -26,7 +26,7 @@ class AddAddressReferenceToSamuCalls extends Migration
     public function down()
     {
         Schema::table('samu_calls', function (Blueprint $table) {
-            //
+            $table->dropColumn('address_reference');
         });
     }
 }
