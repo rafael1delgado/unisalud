a4f049e213ae55f0457d34287a374b25db068b7f
GermanZun <german.zuniga@gmail.com>
2022-04-18 12:55:48 -0400

    subactivityresource

--- app/Http/Resources/MedicalProgrammer/SubActivityResource.php
+++ app/Http/Resources/MedicalProgrammer/SubActivityResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\MedicalProgrammer;
 
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class SubActivityResource extends JsonResource
@@ -9,11 +10,20 @@ class SubActivityResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     * @param  Request  $request
+     * @return array
      */
-    public function toArray($request)
+    public function toArray($request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'specialty_id' => $this->specialty->id_specialty,
+            'profession_id' => $this->profession ?? $this->profession_id,
+            'activity_id' => $this->activity->id_activity,
+            'abbreviation' => $this->sub_activity_abbreviated,
+            'name' => $this->sub_activity_name,
+            'description' => $this->sub_activity_description,
+            'performance' => $this->performance,
+        ];
     }
 }
