f871e68a3b37f6202344662c7d607f95091d1258
AquaroTorres <alvarotorres@gmail.com>
2022-04-21 12:20:46 -0400

    SAMU - Estadistica beta

--- app/Http/Livewire/Samu/Stadistic.php
+++ app/Http/Livewire/Samu/Stadistic.php
@@ -13,37 +13,86 @@ class Stadistic extends Component
     public $communes;
 
     public $key_id;
-    public $kreturn_key_id;
+    public $return_key_id;
     public $commune_id;
     public $from;
     public $to;
 
     public $total = null;
 
-    public $chartData = array();
-    public $chartDataInJson;
+    protected $rules =  [
+        'from' => 'required|date',
+        'to'   => 'required|date'
+    ];
+
+    protected $messages = [
+        'from.required' => 'La fecha desde es obligatoria.',
+        'to.required' => 'La fecha hasta es obligatoria.',
+    ];
 
     public function mount()
     {
+        $this->from      = now()->firstOfMonth()->format('Y-m-d');
+        $this->to        = now()->lastOfMonth()->format('Y-m-d');
         $this->keys      = Key::orderBy('key')->get();
-        $this->communes  = Commune::whereHas('samu')->pluck('id','name')->sort();
-        $this->chartData[] = array("Comuna","Eventos");
+        $this->communes  = Commune::whereHas('samu')->orderBy('name')->pluck('name','id');
     }
 
     public function search()
     {
-        foreach($this->communes as $commune => $key)
+        $this->validate();
+        $chartData = array();
+        $this->total = 0;
+        
+        foreach($this->communes as $id => $name)
         {
-            $this->chartData[] = array($commune, Event::where('commune_id',$key)->count());
-        }
-        $this->total = Event::where('key_id',$this->key_id)->count();
+            $totalEvents = Event::query();
 
-        $this->emit('re-render',$this->chartData);
+            if($this->key_id)
+            {
+                $totalEvents->where('key_id',$this->key_id);
+            }
+
+            $totalEvents->whereBetween('date',[$this->from,$this->to]);
+            $this->total = $totalEvents->count();
+            
+            $totalEvents->where('commune_id',$id);    
+
+            if($totalEvents->count() > 0)
+            {
+                $chartData[] = array($name,$totalEvents->count());
+            }
+        }
+
+        if($this->key_id)
+        {
+            $totalEvents = Event::whereNull('commune_id')
+                ->where('key_id',$this->key_id)
+                ->whereBetween('date',[$this->from,$this->to])
+                ->count();
+        }
+        else
+        {
+            $totalEvents = Event::whereNull('commune_id')
+                ->whereBetween('date',[$this->from,$this->to])
+                ->count();
+        }
+
+        $this->total += $totalEvents;
+
+        if($totalEvents > 0)
+        {
+            $chartData[] = array('Sin comuna',$totalEvents);
+        }
+
+        array_unshift($chartData, ["Comuna", "Eventos"]);
+
+        $this->emit('re-render', $chartData);
     }
 
     public function render()
     {
-        $this->chartDataInJson = json_encode($this->chartData);
+        // $this->chartDataInJson = json_encode($this->chartData);
         return view('livewire.samu.stadistic');
     }
 }
--- resources/views/livewire/samu/stadistic.blade.php
+++ resources/views/livewire/samu/stadistic.blade.php
@@ -7,11 +7,13 @@
         <fieldset class="form-group col-md-2">
             <label for="for-from">Desde</label>
             <input type="date" class="form-control" wire:model="from">
+            @error('from') <span class="text-danger">{{ $message }}</span> @enderror
         </fieldset>
 
         <fieldset class="form-group col-md-2">
             <label for="for-to">Hasta</label>
             <input type="date" class="form-control" wire:model="to">
+            @error('to') <span class="text-danger">{{ $message }}</span> @enderror
         </fieldset>
 
         <fieldset class="form-group col-md-3">
@@ -24,24 +26,40 @@
                 </option>
                 @endforeach
             </select>
+            @error('code') <span class="text-danger">{{ $message }}</span> @enderror
         </fieldset>
 
+        <fieldset class="form-group col-md-3">
+            <label for="for-key">Clave retorno</label>
+            <select class="form-control" wire:model="return_key_id" id="for-return_key_id">
+                <option value="">Selecciona una Clave</option>
+                @foreach($keys as $key)
+                <option value="{{ $key->id }}">       
+                    {{ $key->key }}  - {{ $key->name }}
+                </option>
+                @endforeach
+            </select>
+            @error('code') <span class="text-danger">{{ $message }}</span> @enderror
+        </fieldset>
+
+        {{--
         <fieldset class="form-group col-12 col-md-2">
             <label for="for-commune">Comuna</label>
             <select class="form-control" wire:model="commune_id" id="for-commune">
-                <option value="">Selecciona una Comuna </option>
-                @foreach($communes as $name => $id)
+                <option value="">Todas las comunas</option>
+                @foreach($communes as $id => $name)
                 <option value="{{ $id }}">{{ $name }}</option>
                 @endforeach
             </select>
+            @error('code') <span class="text-danger">{{ $message }}</span> @enderror
         </fieldset>
+        --}}
 
-        <button class="btn btn-primary" wire:click="search">Buscar</button>
     </div>
+    
+    <button class="btn btn-primary" wire:click="search">Buscar</button>
 
-    <pre>
-    Total: {!! $chartDataInJson !!}
-    </pre>
+    <h4>Total de eventos {{ $total }}</h4>
 
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <div id="piechart" style="width: 900px; height: 500px;"></div>
